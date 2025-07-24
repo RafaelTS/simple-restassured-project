3eabfa2bfeeb5814712cde90edac18b34da4ea10
RafaelTS <rafaeltorress@gmail.com>
2025-07-24 16:02:50 -0300

    base struct

---
+++ src/main/java/br/rafael/simple_restassured_project/SimpleRestassuredProjectApplication.java
@@ -0,0 +1,13 @@
+package br.rafael.simple_restassured_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestassuredProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestassuredProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/rafael/simple_restassured_project/SimpleRestassuredProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.rafael.simple_restassured_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestassuredProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
